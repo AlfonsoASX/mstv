b26975221ccf089299da4c0101b595bc1a2ace7a
Alfonso Sánchez <a@asx.mx>
2026-06-01 14:33:44 -0600

    dev

--- control/lib/db.php
+++ control/lib/db.php
@@ -12,6 +12,9 @@ $db_name = "veron119_mstv";
 
 $conexion = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$conexion) {
+    echo 'err:db'. mysqli_connect_error();
     die("Error de conexión a la base de datos: " . mysqli_connect_error());
 }
+
+
 mysqli_set_charset($conexion, "utf8mb4");
